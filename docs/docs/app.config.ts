a1b2d886e2f6d811654bd8f0ec6c78c42fef38a1
jub0t <jforeverything2007@gmail.com>
2024-03-19 20:58:15 +0100

    Update

--- docs/docs/app.config.ts
+++ docs/docs/app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      github: 'jub0t/cdn.rs',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
--- docs/docs/nuxt.config.ts
+++ docs/docs/nuxt.config.ts
@@ -1,7 +1,9 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true
+  },
 
   modules: [
     // Remove it if you don't use Plausible analytics
